c98fd61ee5cf44a5988cc0394b600fd515474164
Caleb Spare <cespare@gmail.com>
2021-08-31 13:51:21 -0700

    Add a script to create releases
    Fixes #31.

---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eu -o pipefail
+
+cd "$(dirname "$0")"
+
+version="$1"
+
+if [[ "$(git rev-parse $version)" != "$(git rev-parse HEAD)" ]]; then
+  echo "Not currently on version $version" 2>&1
+  exit 1
+fi
+
+rm -rf release
+mkdir release
+
+for goos in linux darwin; do
+  for goarch in amd64 arm64; do
+    dir="release/reflex_${goos}_${goarch}"
+    mkdir "$dir"
+    cp LICENSE "${dir}/LICENSE"
+    GOOS=$goos GOARCH=$goarch CGO_ENABLED=0 go build -o "${dir}/reflex"
+    tar -c -f - -C release "$(basename "$dir")" | gzip -9 >"${dir}.tar.gz"
+    rm -rf "${dir}"
+    sha256sum "${dir}.tar.gz" >"${dir}.tar.gz.sha256"
+  done
+done
+
+exec gh release create "$version" \
+  --title "Reflex ${version#v}" \
+  ./release/*.tar.gz \
+  ./release/*.tar.gz.sha256
